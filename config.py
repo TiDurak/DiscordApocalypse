6d7f460490d01371aaaf5ea537d0dc985112f593
TiDurak <bsodrussb@hmail.com>
2022-05-22 20:53:52 +0200

    config fix

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 settings = {
-    'token': os.environ['APOCALYPSE_TOKEN'], # Bot token
-    'bot_user_id': 0,                        # Bot user id
-    'whitelist': [],                         # List of users, who can use this bot
-    'prefix': 'a.'                           # Your prefix
+    'token': os.environ['APOCALYPSE_TOKEN'],         # Bot token
+    'bot_user': "Example#1234",                      # Bot username
+    'whitelist': ["Owner#1337", "OwnerFriend#1338"], # List of users, who can use this bot
+    'prefix': 'a.'                                   # Your prefix
 }
